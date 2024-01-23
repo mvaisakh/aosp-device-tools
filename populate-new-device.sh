59ced8287e0f82a90f5914a5d085b7d5e581f1d2
Vaisakh Murali <vaisakhmurali@gmail.com>
2024-01-23 18:54:42 +0530

    tools: adapt for statix
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- populate-new-device.sh
+++ populate-new-device.sh
@@ -1,18 +1,10 @@
 #!/usr/bin/env bash
 
 # Copyright 2014 The Android Open Source Project
+# Copyright 2024 StatiXOS
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
+# 
 
 if test "$1" = "" -o "$2" = ""
 then
@@ -27,45 +19,27 @@ mkdir -p vendor/$1/$2
 cat > device/$1/$2/AndroidProducts.mk << EOF
 #
 # Copyright 2014 The Android Open-Source Project
+# Copyright 2024 StatiXOS
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# SPDX-License-Identifier: Apache-2.0
+# 
 
-PRODUCT_MAKEFILES := \$(LOCAL_DIR)/full_$2.mk
+PRODUCT_MAKEFILES := \$(LOCAL_DIR)/statix_$2.mk
 
-COMMON_LUNCH_CHOICES := full_$2-userdebug
+COMMON_LUNCH_CHOICES := statix_$2-userdebug
 EOF
 
-cat > device/$1/$2/full_$2.mk << EOF
+cat > device/$1/$2/statix_$2.mk << EOF
 #
 # Copyright 2014 The Android Open-Source Project
+# Copyright 2024 StatiXOS
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# SPDX-License-Identifier: Apache-2.0
+# 
 \$(call inherit-product, \$(SRC_TARGET_DIR)/product/full_base.mk)
 \$(call inherit-product, device/$1/$2/device.mk)
 
-PRODUCT_NAME := full_$2
+PRODUCT_NAME := statix_$2
 PRODUCT_DEVICE := $2
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := $2
@@ -75,19 +49,10 @@ EOF
 cat > device/$1/$2/device.mk << EOF
 #
 # Copyright 2014 The Android Open-Source Project
+# Copyright 2024 StatiXOS
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# SPDX-License-Identifier: Apache-2.0
+# 
 
 ifeq (\$(TARGET_PREBUILT_KERNEL),)
 LOCAL_KERNEL := device/$1/$2-kernel/kernel
@@ -104,19 +69,10 @@ EOF
 cat > device/$1/$2/BoardConfig.mk << EOF
 #
 # Copyright 2014 The Android Open-Source Project
+# Copyright 2024 StatiXOS
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# SPDX-License-Identifier: Apache-2.0
+# 
 
 # Use the non-open-source parts, if they're present
 -include vendor/$1/$2/BoardConfigVendor.mk
@@ -133,36 +89,18 @@ touch device/$1/$2-kernel/MODULE_LICENSE_GPL
 cat > vendor/$1/$2/device-vendor.mk << EOF
 #
 # Copyright 2014 The Android Open-Source Project
+# Copyright 2024 StatiXOS
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# SPDX-License-Identifier: Apache-2.0
+# 
 EOF
 
 cat > vendor/$1/$2/BoardConfigVendor.mk << EOF
 #
 # Copyright 2014 The Android Open-Source Project
+# Copyright 2024 StatiXOS
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# SPDX-License-Identifier: Apache-2.0
+# 
 EOF
 
